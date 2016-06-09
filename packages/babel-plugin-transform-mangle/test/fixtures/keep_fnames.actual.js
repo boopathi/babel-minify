e681448b65d7957363eec0392187e26c3caffc33
Boopathi Rajaa <me@boopathi.in>
2016-06-09 15:36:10 +0200

    A few more tests

--- packages/babel-plugin-transform-mangle/test/fixtures/keep_fnames.actual.js
+++ packages/babel-plugin-transform-mangle/test/fixtures/keep_fnames.actual.js
@@ -1,5 +1,22 @@
-function hello() {}
-var foo = function world() {};
+function function1() {
+  function function2() {
+    function function3() {
+      function function4() {
+        function function5() {
+          function function6() {
+            function function7() {
+              something();
+            }
+          }
+        }
+      }
+    }
+  }
+}
+class Hello {}
+class World extends Hello {}
+
+var foo = function hello_world() {};
 var bar = {
   foo: function baz() {}
 };
