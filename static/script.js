0556210ab657eea6203c206b6a8061470d3e0abd
Boopathi Rajaa <me@boopathi.in>
2016-06-14 23:59:30 +0200

    improve some ux

--- static/script.js
+++ static/script.js
@@ -3,7 +3,21 @@
   var output = document.getElementById('output');
   var run = document.getElementById('run');
 
+  var readyText = run.innerHTML;
+  var loadingText = "<small>loading</small> ...";
+
+  function enableRun() {
+    run.innerHTML = readyText;
+    run.disabled = false;
+  }
+  function disableRun() {
+    run.innerHTML = loadingText;
+    run.disabled = true;
+  }
+
   var minifier = new Worker('static/worker.js');
+
+  disableRun();
 
   run.addEventListener('click', function(e) {
     e.preventDefault();
@@ -14,6 +28,11 @@
   });
 
   minifier.addEventListener('message', function(out) {
+    if (out.data.ready) return enableRun();
     output.value = out.data;
   });
+
+
+  // kick start
+  minifier.postMessage({ start: true });
 })();
--- static/worker.js
+++ static/worker.js
@@ -1,6 +1,13 @@
 importScripts('../build/babel-minify.js');
 
 self.addEventListener('message', function(e) {
+
+  if (e.data.start) {
+    return self.postMessage({
+      ready: true
+    });
+  }
+
   var input = e.data.input;
   var opts = e.data.opts;
   var result = BabelMinify(input, opts);
