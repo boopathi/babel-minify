cb5fe5724b0de3ca89867f228d3edf15f24b2d74
Boopathi Rajaa <me@boopathi.in>
2016-06-14 22:11:58 +0200

    Remove mockability of fs - never required

--- packages/babel-minify/src/cli.js
+++ packages/babel-minify/src/cli.js
@@ -1,13 +1,7 @@
-/* eslint-disable no-console */
-import fs from 'fs';
-
 import yargs from './yargs';
 import {parseArgv} from './parse-argv';
 import {runTasks} from './tasks';
 
-/**
- * Runner
- */
 function run(argvRaw, opts) {
   const argv = yargs(argvRaw);
   parseArgv(argv, opts);
@@ -19,7 +13,6 @@ module.exports = run;
 // I don't know why I'm doing this
 if (require.main === module) {
   run(process.argv.slice(2), {
-    fs,
     logger: console
   });
 }
--- packages/babel-minify/src/tasks.js
+++ packages/babel-minify/src/tasks.js
@@ -1,17 +1,18 @@
+import fs from 'fs';
 import path from 'path';
 import minify from '../';
 
 export function getFilesAndMinify(argv, opts) {
   argv.results = argv._
     .map(fileOrDir => {
-      if (opts.fs.statSync(fileOrDir).isDirectory()) {
+      if (fs.statSync(fileOrDir).isDirectory()) {
         opts.logger.error('Directories are not supported.');
         throw new Error('Input file is a Directory');
       }
       return fileOrDir;
     })
     .map(file => ({
-      contents: String(opts.fs.readFileSync(file)),
+      contents: String(fs.readFileSync(file)),
       filename: file
     }))
     .map(({contents, filename}) => ({
@@ -25,13 +26,13 @@ export function putFiles(argv, opts) {
     .forEach(({contents, filename}) => {
       if (argv._.length === 1) {
         if (argv.output)
-          opts.fs.writeFileSync(argv.output, contents);
+          fs.writeFileSync(argv.output, contents);
         else
           opts.logger.log(contents);
       } else {
         if (argv.outputDir) {
           const basename = path.basename(filename);
-          opts.fs.writeFileSync(path.join(argv.outputDir, basename), contents);
+          fs.writeFileSync(path.join(argv.outputDir, basename), contents);
         } else {
           opts.logger.error('Output Directory unspecified');
           throw new Error('Output Directory unspecified');
