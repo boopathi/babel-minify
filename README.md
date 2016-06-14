b7088e2746fd56ed1f34d32653f070f3beaa58c3
Boopathi Rajaa <me@boopathi.in>
2016-06-14 21:53:23 +0200

    Fresh GH-PAGES

--- README.md
+++
@@ -1,7 +0,0 @@
-# babel-minify
-
-Some babel plugins and presets to minify.
-
-## babel-preset-min
-
-## babel-plugin-transform-shorten-identifiers
--- utils/index.js
+++
@@ -1,7 +0,0 @@
-// utils for tests
-export function toComparable(str) {
-  return str.replace(/[;\s\f\r\n\t\v]/g, '');
-}
-export function compare(code1, code2) {
-  return toComparable(code1) === toComparable(code2);
-}
