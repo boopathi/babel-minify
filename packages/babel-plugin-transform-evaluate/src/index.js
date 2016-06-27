7ec9fb6b329a4ef0d81f19fb73ad6d3f754ba9ee
Boopathi Rajaa <me@boopathi.in>
2016-06-27 22:12:02 +0200

    Add some dead_code removal stuff to evaluate

--- packages/babel-plugin-transform-evaluate/src/index.js
+++ packages/babel-plugin-transform-evaluate/src/index.js
@@ -7,6 +7,7 @@ export default function({types: t}) {
           if (evaluated.confident) path.replaceWith(t.valueToNode(evaluated.value));
         }
       },
+
       AssignmentExpression(path) {
         const left = path.get('left');
         if (!left.isIdentifier()) return;
@@ -21,29 +22,74 @@ export default function({types: t}) {
           binding.deoptValue();
         }
       },
-      Conditional(path) {
+
+      Conditional: function Conditional(path) {
         const evaluated = path.get('test').evaluate();
         if (!evaluated.confident) {
           return path.skip();
         }
-
-        function replaceWith(to) {
-          const toStatement = path.get(to);
-          if (!toStatement.isBlockStatement()) {
-            return path.replaceWith(toStatement);
-          }
-          if (Object.keys(toStatement.scope.bindings).length > 0) {
-            return path.replaceWith(toStatement);
-          }
-          path.replaceWithMultiple(toStatement.node.body);
-        }
-
         if (evaluated.value) {
           replaceWith('consequent');
         } else {
           replaceWith('alternate');
         }
+
+        function getVars(blockPath) {
+          const declarations = [];
+          if (blockPath.isBlockStatement()) {
+            const blockFnScope = blockPath.getFunctionParent();
+            blockPath.traverse({
+              VariableDeclaration: function VariableDeclaration(varPath) {
+                if (varPath.isVariableDeclaration({kind:'var'}) && varPath.getFunctionParent() === blockFnScope) {
+                  declarations.push(varPath);
+                }
+              }
+            });
+          } else if (blockPath.isVariableDeclaration({ kind: 'var' })) {
+            declarations.push(blockPath);
+          }
+          return declarations;
+        }
+
+        // needs a better function name
+        function dontForgetAlternate(blockPath) {
+          const declarators = [];
+
+          getVars(blockPath).forEach(decl => {
+            decl.get('declarations').forEach(dr => {
+              declarators.push(t.variableDeclarator(dr.node.id));
+            });
+          });
+
+          if (declarators.length <= 0) return null;
+          return t.variableDeclaration('var', declarators);
+        }
+
+        function replaceWith(to) {
+          const remove = to === 'consequent' ? 'alternate' : 'consequent';
+          const replacements = [];
+
+          const toBlock = path.get(to);
+          const removeBlock = path.get(remove);
+
+          const decl = dontForgetAlternate(removeBlock);
+          if (decl) replacements.push(t.BlockStatement([decl]));
+
+          if (toBlock.isBlockStatement()) {
+            if (Object.keys(toBlock.scope.bindings).length > 0 || getVars(toBlock).length > 0) {
+              replacements.push(toBlock.node);
+            } else {
+              toBlock.node.body.forEach(e => replacements.push(e));
+            }
+          } else if (toBlock.isVariableDeclaration()) {
+            replacements.push(t.BlockStatement([toBlock.node]));
+          } else if (toBlock.node !== null) {
+            replacements.push(toBlock.node);
+          }
+          path.replaceWithMultiple(replacements);
+        }
       },
+
       Scopable: {
         enter(path) {
           const funcScope = path.scope.getFunctionParent();
--- utils/try.js
+++ utils/try.js
@@ -2,7 +2,7 @@
 const plugin = require('../packages/babel-plugin-transform-evaluate');
 const babel = require('babel-core');
 
-const input = `
+let input = `
 function a() {
   var x = 5;
   let y = 6;
@@ -11,21 +11,54 @@ function a() {
     y = x % 2 === 0 ? x : x + 5;
   }
   console.log(x+6);
+
   var NODE_ENV = 'production';
+
   if (NODE_ENV !== 'production') {
     console.log('dev');
+    function abc() {
+      console.log('whatever');
+    }
   } else if (false) {
+    var abcd = "Asdf";
+  } else if (false) {
+    var aaaa = "asdfsdfasdf";
+    function whatever() {
+      console.log('whatever');
+    }
+  }
+  else if (false) {
     let x = 5;
     console.log('blahblah');
   } else {
     console.log('neither');
   }
+
+  if (true) {
+    var a = 5;
+  }
 }
 `;
+
+// input = `
+// if (false) {
+//   let x = 5;
+//   var a = 1;
+//   {
+//     var z = 6;
+//   }
+//   function c() {
+//     var a = 1;
+//   }
+// }
+// `
 
 const output = babel.transform(input, {
   babelrc: false,
   plugins: [plugin]
 });
 
-console.log(input, output.code);
+console.log('Input:');
+console.log(input);
+console.log('Output:');
+console.log(output.code);
