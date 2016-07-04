5fdae6c46d18b8b962cef14d7cb0ad8973f1b0ce
Boopathi Rajaa <me@boopathi.in>
2016-07-04 11:11:43 +0200

    make it work in safari

--- static/script.js
+++ static/script.js
@@ -12,7 +12,7 @@
   ace.config.set('workerPath', 'https://cdn.jsdelivr.net/ace/1.2.3/min/');
   ace.config.set('themePath', 'https://cdn.jsdelivr.net/ace/1.2.3/min/');
   var editor = ace.edit('input');
-  editor.$blockScrolling = Infinity;
+  // editor.$blockScrolling = Infinity;
   editor.getSession().setMode('ace/mode/javascript');
   editor.getSession().setOptions({ tabSize: 2, useSoftTabs: true });
 
