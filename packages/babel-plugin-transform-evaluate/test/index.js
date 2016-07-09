66b8502778689ea870c885711409b1a4a1de92fc
Boopathi Rajaa <me@boopathi.in>
2016-07-09 18:35:50 +0200

    Restructure evaluate tests to match the testing style followed in other packages

--- packages/babel-plugin-transform-evaluate/test/index.js
+++ packages/babel-plugin-transform-evaluate/test/index.js
@@ -1,49 +1,58 @@
 import evaluate from '../src';
 
-const babelOpts = {
-  babelrc: false,
-  plugins: [evaluate]
-};
+function test(input) {
+  return trim(transform(input, {
+    plugins: [evaluate],
+    babelrc: false,
+  }).code);
+}
 
-describe('babel-plugin-transform-evaluate', function() {
-  it('should transform binary expressions', function() {
-    const actual = transform(`
-      var x = 1+2+3+4%5;
-    `, babelOpts).code
-    const expected = `var x = 10`;
-    expect(trim(actual)).toEqual(trim(expected));
-  });
-  it('should transform logical expressions', function() {
-    const actual = transform(`
-      const y = true && false || "default value";
-    `, babelOpts).code
-    const expected = `const y = "default value"`;
-    expect(trim(actual)).toEqual(trim(expected));
-  });
-  it('should transform simple call expressions', function() {
-    const actual = transform(`
-      var x = 1+2+3+4%5+Math.ceil(4.5);
-      function y() { return 4 };
-      y();
-    `, babelOpts).code
-    const expected = `
-      var x = 15;
-      function y() {return 4};
-      y();
-    `;
-    expect(trim(actual)).toEqual(trim(expected));
-  });
-  it('should not transform (1, func)(something) to func(something)', function() {
-    const actual = transform(`
-      (1, eval)('x=4')
-    `, babelOpts).code
-    const expected = `(1,eval)('x=4')`;
-    expect(trim(actual)).toEqual(trim(expected));
+describe('babel-plugin-transform-evaluate', function () {
+  it('should transform binary expressions', function () {
+    expect(
+      test('var x = 1 + 2 + 3 + 4 % 5;')
+    ).toEqual(
+      trim('var x = 10')
+    );
   });
 
-  it('should not fail to deopt for object and array pattern', function() {
-    const actual = transform('var x = 5; var [y] = [x]; { x = 6 }', babelOpts).code;
-    const expected = 'var x = 5; var [y] = [x]; { x = 6 }';
-    expect(trim(actual)).toEqual(trim(expected));
+  it('should transform logical expressions', function () {
+    expect(
+      test('const y = true && false || "default value";')
+    ).toEqual(
+      trim('const y = "default value"')
+    );
+  });
+
+  it('should transform simple call expressions', function () {
+    expect(
+      test(`
+        var x = 1+2+3+4%5+Math.ceil(4.5);
+        function y() { return 4 };
+        y();
+      `)
+    ).toEqual(
+      trim(`
+        var x = 15;
+        function y() {return 4};
+        y();
+      `)
+    );
+  });
+
+  it('should not transform (1, func)(something) to func(something)', function () {
+    expect(
+      test('(1, eval)("x=4")')
+    ).toEqual(
+      trim('(1, eval)("x=4")')
+    );
+  });
+
+  it('should not fail to deopt for object and array pattern', function () {
+    expect(
+      test('var x = 5; var [y] = [x]; { x = 6 }')
+    ).toEqual(
+      trim('var x = 5; var [y] = [x]; { x = 6 }')
+    );
   });
 });
